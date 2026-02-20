6bd2b4a76564d005463b0d25a0d1c9967b57afcd
Ryan Vu <vuryan612@gmail.com>
2026-02-19 21:34:18 -0800

    board generation generates boards that dont have optimal solution of depth

--- project1/Puzzle.java
+++ project1/Puzzle.java
@@ -17,122 +17,171 @@ public class Puzzle {
             System.out.println("Select:\n[1] Single Test Puzzle\n[2] Multi-Test Puzzle\n[3] Exit");
             int test = scanner.nextInt();
 
-            if (test == 1) {
-                // Single Test Puzzle
-            } else if (test == 2) {
-                // Multi-Test Puzzle
-
-            } else if (test == 3) {
-                // Exit
-                scanner.close();
-                return;
-            } else {
-                System.out.println("Invalid input.");
-                continue;
+            int numberOfTests;
+            while (true) {
+                if (test == 1) {
+                    // Single Test Puzzle
+                    numberOfTests = 1;
+                    break;
+                } else if (test == 2) {
+                    // Multi-Test Puzzle
+                    System.out.println("Enter number of tests: (1 - 100)");
+                    numberOfTests = scanner.nextInt();
+                    if (numberOfTests < 1 || numberOfTests > 100) {
+                        System.out.println("Invalid input.");
+                        continue;
+                    }
+                    break;
+                } else if (test == 3) {
+                    // Exit
+                    scanner.close();
+                    return;
+                } else {
+                    System.out.println("Invalid input.");
+                    continue;
+                }
             }
 
             System.out.println("Select Input Method:\n[1] Random\n[2] File");
             int method = scanner.nextInt();
 
-            if (method == 1) {
-                // Random
-            } else if (method == 2) {
-                // File
-
-            } else {
-                System.out.println("Invalid Input.");
-                continue;
-            }
-
-            System.out.println("Select Solution Depth (2-20");
-            int depth = scanner.nextInt();
-
-            Node currentNode = generateRandomBoard(depth);
-            printBoard(currentNode.getBoard());
-
-            if (!checkSolvability(currentNode.getBoard())) {
-                System.out.println("Board is not solvable");
-                continue;
-            }
-            
-            Map<String, Integer> gValue = new HashMap<>();      // board -> g value
-            Map<String, String> parentMap = new HashMap<>();    // board -> parent board
-
-            // Change comparator to use F
-            PriorityQueue<Node> priorityQueue = new PriorityQueue<>((a, b) -> a.getF() - b.getF());
-
-            priorityQueue.add(currentNode);
-            gValue.put(currentNode.getBoard(), currentNode.getG());
-            String parentBoard = currentNode.getBoard();
-            parentMap.put(currentNode.getBoard(), null);    // no parent to the initial
-
-            // AStar algorithm. Will go until no more states to visit or goal is met.
-            // When goal is met, break.
-            while (!priorityQueue.isEmpty()) {
-                currentNode = priorityQueue.poll();
-                if (currentNode.getG() > gValue.get(currentNode.getBoard())) {
-                    continue; // outdated version of this board
-                }
-
-                // check each direction
-                if (currentNode.getH() == 0) {
+            while (true) {
+                if (method == 1) {
+                    // Random
                     break;
+                } else if (method == 2) {
+                    // File
+                    break;
+                } else {
+                    System.out.println("Invalid Input.");
+                    continue;
                 }
+            }
 
-                int zeroRow = currentNode.getZeroRow();
-                int zeroCol = currentNode.getZeroCol();
-                
-                for (Direction d: Direction.values()) {
-                    // make sure not on border
-                    if (zeroRow + d.dr >= 0 && zeroRow + d.dr <= 2 && zeroCol + d.dc >= 0 && zeroCol + d.dc <= 2) {
-                        Node node = generateNeighborNode(currentNode, d);
+            System.out.println("Select Solution Depth (2-20)");
+            int depth = scanner.nextInt();
+            Node initialBoard = generateRandomBoard(depth);
 
-                        // If repeated node, replace key with smaller g value. Otherwise, add to hashmap.
-                        // In the case of repeated but larger g value, don't do anything.
-                        if (!gValue.containsKey(node.getBoard())) {
-                            gValue.put(node.getBoard(), node.getG());
-                            parentMap.put(node.getBoard(), currentNode.getBoard());
-                        } else if (node.getG() < gValue.get(node.getBoard())) {
-                            gValue.put(node.getBoard(), node.getG());
-                            parentMap.put(node.getBoard(), currentNode.getBoard());
+            System.out.println("Puzzle:");
+            printBoard(initialBoard.getBoard());
+
+            System.out.println("Select H Function:\n[1] H1\n[2] H2");
+            int hFunction = scanner.nextInt();
+
+            while (true) {
+                if (hFunction == 1) {
+                    break;
+                } else if (hFunction == 2) {
+                    if (numberOfTests == 1) {
+                        aStarH2(initialBoard, true);
+                    } else {
+                        int totalSearchCost = 0;
+                        for (int i = 0; i < numberOfTests; i++) {
+                            Node board = generateRandomBoard(depth);
+                            totalSearchCost += aStarH2(board, false);
                         }
-                        // add both old and new nodes. the ones with smaller g will be skipped later on.
-                        priorityQueue.add(node);
+                        System.out.println("Average Search Cost: " + totalSearchCost);
                     }
+                    break;
+                } else if (hFunction == 3) {
+                    break;
+                } else {
+                    System.out.println("Invalid input");
+                    continue;
                 }
             }
-
-            // currentNode now holds the node with goal. 
-            // Need to figure out the path by retracing parents.
-            String sBoard = currentNode.getBoard();
-            ArrayList<String> path = new ArrayList<>();
-
-            while (sBoard != null) {
-                path.add(sBoard);
-                sBoard = parentMap.get(sBoard);
-            }
-
-            // add parent
-            path.add(parentBoard);
-
-            // Reverse path to get from original path to goal
-            Collections.reverse(path);
-
-            // Print path :D
-            for (String s : path) {
-                System.out.println("=============");
-                printBoard(s);
-            }
-
-            System.out.println("Total moves: " + path.size());
         }
     }  
 
-    public static int aStarH1(String board) {
+    public static int aStarH1Individual(String board) {
         return 0;
     }
 
-    public static int aStarH2(String board) {
+    public static int aStarH2(Node node, boolean individual) {
+        Node currentNode = node;
+        int count = 0;
+        printBoard(currentNode.getBoard());
+
+        if (!checkSolvability(currentNode.getBoard())) {
+            System.out.println("Board is not solvable");
+        }
+        
+        Map<String, Integer> gValue = new HashMap<>();      // board -> g value
+        Map<String, String> parentMap = new HashMap<>();    // board -> parent board
+
+        // Change comparator to use F
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<>((a, b) -> a.getF() - b.getF());
+
+        priorityQueue.add(currentNode);
+        gValue.put(currentNode.getBoard(), currentNode.getG());
+        String parentBoard = currentNode.getBoard();
+        parentMap.put(currentNode.getBoard(), null);    // no parent to the initial
+
+        // AStar algorithm. Will go until no more states to visit or goal is met.
+        // When goal is met, break.
+        while (!priorityQueue.isEmpty()) {
+            currentNode = priorityQueue.poll();
+            if (currentNode.getH() == 0) {
+                break;
+            }
+
+            if (currentNode.getG() > gValue.get(currentNode.getBoard())) {
+                continue; // outdated version of this board
+            }
+
+            int zeroRow = currentNode.getZeroRow();
+            int zeroCol = currentNode.getZeroCol();
+            
+            for (Direction d: Direction.values()) {
+                // make sure not on border
+                if (zeroRow + d.dr >= 0 && zeroRow + d.dr <= 2 && zeroCol + d.dc >= 0 && zeroCol + d.dc <= 2) {
+                    Node neighborNode = generateNeighborNode(currentNode, d);
+                    count++;
+
+                    // If repeated node, replace key with smaller g value. Otherwise, add to hashmap.
+                    // In the case of repeated but larger g value, don't do anything.
+                    if (!gValue.containsKey(neighborNode.getBoard())) {
+                        gValue.put(neighborNode.getBoard(), neighborNode.getG());
+                        parentMap.put(neighborNode.getBoard(), currentNode.getBoard());
+                    } else if (neighborNode.getG() < gValue.get(neighborNode.getBoard())) {
+                        gValue.put(neighborNode.getBoard(), neighborNode.getG());
+                        parentMap.put(neighborNode.getBoard(), currentNode.getBoard());
+                    }
+                    // add both old and new nodes. the ones with smaller g will be skipped later on.
+                    priorityQueue.add(neighborNode);
+                }
+            }
+        }
+
+        // currentNode now holds the node with goal. 
+        // Need to figure out the path by retracing parents.
+        String sBoard = currentNode.getBoard();
+        ArrayList<String> path = new ArrayList<>();
+
+        while (sBoard != null) {
+            path.add(sBoard);
+            sBoard = parentMap.get(sBoard);
+        }
+
+        // add parent
+        path.add(parentBoard);
+
+        // Reverse path to get from original path to goal
+        Collections.reverse(path);
+
+        // Print path :D
+        if (individual) {
+            for (int i = 0; i < path.size(); i++) {
+                System.out.println("Step " + (i + 1));
+                printBoard(path.get(i));
+                System.out.println();
+            }
+        }
+
+        return count;
+    }
+
+    public static int aStarH2Multiple(int depth) {
         return 0;
     }
 
@@ -145,30 +194,26 @@ public class Puzzle {
         int zeroCol; 
 
         Direction[] directions = Direction.values();
-        int size = directions.length;
-        Direction direction;
 
         for (int i = 0; i < depth; i++) {
-            previousBoards.add(currentBoard.getBoard());
+            while (true) {
+                Direction direction = directions[ThreadLocalRandom.current().nextInt(directions.length)];
 
-            zeroRow = currentBoard.getZeroRow();
-            zeroCol = currentBoard.getZeroCol();
+                zeroRow = currentBoard.getZeroRow();
+                zeroCol = currentBoard.getZeroCol();
 
-            // Randomize direction for next board
-            do {
-                direction = directions[ThreadLocalRandom.current().nextInt(size)];
-                if (!(zeroRow + direction.dr >= 0 && zeroRow + direction.dr <= 2 && zeroCol + direction.dc >= 0 && zeroCol + direction.dc <= 2)) {
-                    continue;
-                } 
-                Node node = generateNeighborNode(currentBoard, direction);
-                if (previousBoards.contains(node.getBoard())) {
-                    continue;
-                }
+                int nRow = zeroRow + direction.dr;
+                int nCol = zeroCol + direction.dc;
 
-                currentBoard = node;
+                if (nRow < 0 || nRow > 2 || nCol < 0 || nCol > 2) continue;
+
+                Node nextNode = generateNeighborNode(currentBoard, direction);
+                if (previousBoards.contains(nextNode.getBoard())) continue;
+
+                currentBoard = nextNode;
                 previousBoards.add(currentBoard.getBoard());
                 break;
-            } while (true);
+            }
         }
 
         return currentBoard;
