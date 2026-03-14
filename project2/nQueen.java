370421114c7366554fd2378bace257994103f832
Ryan Vu <vuryan612@gmail.com>
2026-03-14 15:34:59 -0700

    use List of arrays to be able to track where queens are

--- project2/nQueen.java
+++ project2/nQueen.java
@@ -1,5 +1,7 @@
 package project2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class nQueen {
@@ -7,7 +9,11 @@ public class nQueen {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number of queens: ");
         int n = scanner.nextInt();
-        boolean[][] board = new boolean[n][n];
+        List<int[]> queens = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            queens.add(new int[n]);
+        }
 
         // Randomize queen positions
         int i = 0;
@@ -15,19 +21,24 @@ public class nQueen {
             int row = (int) (Math.random() * (n));
             int col = (int) (Math.random() * (n));
 
-            if (!board[row][col]) {
-                board[row][col] = true;
+            if (queens.get(row)[col] == 0) {
+                queens.get(row)[col] = 1;
                 i++;
             } 
         }  
 
-        printBoard(board);
+        printBoard(queens);
     }
 
-    public static void printBoard(boolean[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                System.out.print(board[i][j] + "\t");
+    public static boolean checkAttack(){
+
+        return false;
+    }
+
+    public static void printBoard(List<int[]> queens) {
+        for (int i = 0; i < queens.size(); i++) {
+            for (int j = 0; j < queens.size(); j++) {
+                System.out.print(queens.get(i)[j] + " ");
             }
             System.out.println();
         }
